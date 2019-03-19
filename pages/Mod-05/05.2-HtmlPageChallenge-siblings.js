e0ff1c235629d7c6ea07c27616ec17d7956c31c1
Makeuu <mattdu77@live.fr>
2019-03-19 14:52:22 +0100

    MOD 5 - TP 5 JQUERY pour mettre en valeur des images + récupération des infos

--- pages/Mod-05/05.2-HtmlPageChallenge-siblings.js
+++ pages/Mod-05/05.2-HtmlPageChallenge-siblings.js
@@ -5,16 +5,43 @@
 $(document).ready(function () {
 
     //Action 1 : Images opacity
+    $('.dcell>img').css("opacity", "0.4");
 
     //Action 2 : Abonnements des images du formulaire
+    $('.dcell>img')
+        .mouseenter(MouseEnterHandler)
+        .mouseout(MouseOutHandler);
 
     //Action 3 : coder le gestionnaire MouseEnter 
     function MouseEnterHandler() {
+        //css
+        var objCss = {
+            'font-size':'larger',
+            'font':'bold',
+            'color': 'black',
+            'background-color': 'orange'
+        }
+        $(this).css('opacity', '1').addClass('anim');
+        $(this).siblings('input').css(objCss);
         
+
+        var test = $('#divInfo');
+        $('#infoGif').show();
+        $('#info').html('Consultation du produit : '
+            + $(this).siblings('label').text()
+            + ' du rayon '
+            + $(this).parents('.drow').attr('id')
+            //+ $(this).parentsUntil('.dtable').attr('id')
+            + 'avec une commande en cours de '
+            + $(this).siblings('input').val()
+        )
     };
 
     //Action 4 : coder le gestionnaire MouseOut
     function MouseOutHandler() {
-        
+        $(this).css('opacity', 0.4).removeClass('anim');
+        $(this).siblings('input').css('background-color', '');
+        $('#info').empty().removeClass('input');
+        $('#infoGif').hide();
     };
 });
