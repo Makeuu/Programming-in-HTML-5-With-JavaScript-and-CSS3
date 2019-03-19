f8f3b0aea745088defaf652dcf4de8479c43a265
Makeuu <mattdu77@live.fr>
2019-03-19 17:31:43 +0100

    MOD 6 - TP6 Ajout jquery validation dans formulaire

--- pages/Mod-06/06.3-HtmlPageFormOrder.js
+++ pages/Mod-06/06.3-HtmlPageFormOrder.js
@@ -9,42 +9,112 @@ $(document).ready(function () {
     $('.dcell>img')
         .mouseenter(MouseEnterHandler)
         .mouseout(MouseOutHandler)
-        
-    
+        .click(MouseClick);
+
+
     //Abonnement à la perte de focus et au changement dans la TextBox
     //Vérifie si une commande est passée
-   
+    $('.dcell>input').on('keyup change', function(e){
+        //Tester si il y a plusieurs commandes
+        testInputValue();
+        //Activation du bouton commande
+        //Test disabled via css bootstrap sur le bouton btnOrdedr
+        if(newItems > 0){
+            $('#btnOrder').removeClass('btn btn-info disabled');
+            $('#btnOrder').addClass('btn btn-default');
+        }
+        else{
+            $('#btn Order').removeClass('btn btn-default');
+            $('#btnOrder').addClass('btn btn-info disabled');
+        }
+    });
 
     //Abonnement au focus : seléctionner le nombre déjà présent
-    
-
+    $('.dcell>input').on('focus', function (e){
+        this.select();
+        this.focus();
+    });
 
     //Tester la valeur des TextBox (imput)
-    
+    var newItems = 0;
+    function testInputValue(){
+        newItems = 0;
+        var result = $('.dcell>input').each(function (index, elem){
+            var tot = parseInt($(elem).val());
+            //Si jamais le input contient autre chose qu'un nomnre
+            //onpourrait via onkey desactiver toute entée différente d'un nombre
+            //on pourrait aussi utiliser un input type = number
+            if(!Number.isNaN(tot)){
+                if(tot > 0){
+                    newItems += tot;
+                }
+            }
+        });
+    }
 
     //Abonnement au click du bouton Order
     //Affichage des commandes 
-    
+    $('#btnOrder').click(function(evt){
+        testInputValue();
+        console.log(newItems);
+        if(newItems <= 0) {
+            alert('Votre panier est vide !!');
+            //evt.preventDefault();
+        } else {
+            //Commande globale le total de livres
+            alert("Nous enregistrons votre commande pour un total de "
+                + newItems + " livres jQuery!");
+            //Détail de la commande globale
+            DetailCommande();
+            Commandes.forEach(function(el,i){
+                alert(el.tot + " livre(s) " + el.name);
+            })
+            //Remettre les input à 0 pour une nouvelle commande
+            //Vider le tableau de commande
+            //Remettre le bouton à disable
+            $('.dcell>input').val(0);
+            Commandes = [];
+            $('#btn Order').removeClass('btn btn-default');
+            $('#btnOrder').addClass('btn btn-info disabled');
+        }
+        evt.preventDefault();
+    })
 
-    
+
+
     //Objet JavaScript : représente notre modèle de commande
     var CommandeObject = function (name, tot) {
         this.name = name,
-        this.tot = tot
+            this.tot = tot
     }
 
     //Tableau des commandes 
     var Commandes = [];
     //Détail de la commande (itérer chaque input et récupérer le label associé)
-   
-
-
+    function DetailCommande(){
+        var result = $('.dcell>input');
+        for(var i = 0; i < result.length; i++){
+            var tot = $(result[i]).val();
+            if(tot > 0){
+                var elName = $(result[i]).siblings('label').text()
+                var cmd = new CommandeObject(elName, tot);
+                //Commande[elName] = tot;
+                //Commande.elName = tot;
+                Commandes.push(cmd);
+            }
+        }
+    }
 
     //MouseClick
     //Incrémenter la commande
     //Déclencher l'événement blur afin de vérifier si le bouton order peut être activé
     var i = 0;
-    
+    function MouseClick(){
+        //Incrementer les inputs
+        $(this).siblings('input').val(++i).trigger('blur');
+        //Afficher le div du footer et resyet en phase avec l'affichage
+        $(this).trigger("mouseenter");
+    }
 
     //MouseEnterHandler
     function MouseEnterHandler() {
@@ -73,8 +143,9 @@ $(document).ready(function () {
 
     //Désactiver l'événement enter sur input afin de ne pas déclencher
     //l'événement click du bouton
-    
-
+    $('form input').on('keyup keypress', function(e){
+        return e.which !== 13;
+    });
 
 });
 
