42576db4d74fb50887f16dbf9a9f6167758f2cef
Makeuu <mattdu77@live.fr>
2019-03-20 10:19:01 +0100

    MOD 6 - Ajout des validations sur le formulaire

---
+++ pages/Mod-06/06.2-ValidationForm.js
@@ -0,0 +1,55 @@
+﻿/// <reference path="../../scripts/jquery-1.10.2.js" />
+/// <reference path="../../scripts/jquery.validate.js" />
+
+
+function ValidateForm() {
+    $.validator.addMethod('noJames', function(value, element){
+        return (!value.endsWith('007'));
+    })
+
+    $('#MyForm').validate(
+        //Règles
+        {
+            rules:{
+                firstname:{
+                    required: true,
+                    noJames: true,
+                    minlength: 4
+                },
+                email:{
+                    required: true,
+                    email: true
+                },
+            },
+            messages:{
+                firstname:{
+                    required: "Saisir un nom ",
+                    noJames: "Pas de 007",
+                    minlength: "Minimum 4 caractères"
+                },
+                email:{
+                    required: "Saisir un email ",
+                    email: "Votre adresse mail n'est pas valide"
+                }
+            },
+            submitHandler: function(form){
+                alert("Ajax");
+                var test = $(form).serializeArray();
+                console.log(test);
+            }
+        }
+    )
+}
+
+$(document).ready(function () {
+    //Action sur le DOM
+    ValidateForm();
+    //Test sur le Submit Input
+    $('input').on('blur', function(){
+        if($('#MyForm').valid()){
+            $('#submit').prop('disabled', false);
+        }else{
+            $('#submit').prop('disabled', 'disabled');
+        }
+    })
+});
